b585b65f1cfab918f09329b48c0c598ec773caa1
Georg <giony.mirca@gmail.com>
2021-09-07 23:32:28 +0300

    asynchronous added

--- index.py
+++ index.py
@@ -3,9 +3,12 @@ from requests.auth import HTTPBasicAuth
 from pymongo import MongoClient
 import requests
 import pymongo
+import aiohttp
+import asyncio
 import json
 import os
 import re
+import time
 from flask import Response
 
 cluster = MongoClient('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false')
@@ -16,14 +19,15 @@ app = Flask(__name__)
 db = cluster['test']
 collection = db['test']
 
+
 @app.route('/get_adverts')
 def step_one():
     r = requests.get('https://partners-api.999.md/adverts?page_size=10&page=1', auth=HTTPBasicAuth('apuUo-UF6yhfoNVVTKWrb5Z8ecru', ''))
     return r.json()
 
 
-@app.route('/db')
-def step_two():
+@app.route('/save_adverts_to_db')
+def save_adverts_to_db():
     r = requests.get('https://partners-api.999.md/adverts?page_size=10&page=1', auth=HTTPBasicAuth('apuUo-UF6yhfoNVVTKWrb5Z8ecru', ''))
     collection.insert_one(r.json())
     return 'success'
@@ -35,16 +39,32 @@ def get_eur_price():
     return float(x)
 
 
-@app.route('/convert')
-def step_three():
+async def get(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, auth=aiohttp.BasicAuth('apuUo-UF6yhfoNVVTKWrb5Z8ecru', '')) as response:
+            data = await response.read()
+            return data
+
+
+@app.route('/convert_and_save')
+def convert_and_save():
     eur_price = get_eur_price()
     json_data = db.test.find_one()
 
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    coroutines = []
+
     for advert in json_data['adverts']:
         id = advert['id']
-        r = requests.get(f'https://partners-api.999.md/adverts/{id}?lang=ru', auth=HTTPBasicAuth('apuUo-UF6yhfoNVVTKWrb5Z8ecru', ''))
-        json_data = r.json()
+        coroutines.append(get(f'https://partners-api.999.md/adverts/{id}?lang=ru'))
 
-        if json_data['price']['unit'] == 'eur':
-            actual_price = float(json_data['price']['value']) * eur_price
-            print("{:.4f}".format(actual_price))
+    result = loop.run_until_complete(asyncio.gather(*coroutines))
+
+    for advert in result[:2]:
+        print('-------')
+        data = json.loads(advert)
+        if data['price']['unit'] == 'eur':
+            data['price']['unit'] = 'mdl'
+            data['price']['value'] = float(data['price']['value']) * eur_price
+        collection.insert_one(data)
